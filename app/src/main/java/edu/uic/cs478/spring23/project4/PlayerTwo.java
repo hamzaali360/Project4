57c0d62d56783fc8242568712b321c8e89f6db26
hamzaali360 <hamali1708@gmail.com>
2023-04-08 16:55:23 -0500

    work in progress

--- app/src/main/java/edu/uic/cs478/spring23/project4/PlayerTwo.java
+++ app/src/main/java/edu/uic/cs478/spring23/project4/PlayerTwo.java
@@ -20,15 +20,15 @@ public class PlayerTwo implements Runnable{
             public void handleMessage(Message msg) {
                 switch (msg.what) {
                     case MOVE_MADE:
-                        updateBoard(new PieceData(msg.arg1,msg.arg2,1),(PieceData)msg.obj)
+                        updateBoard(new PieceData(msg.arg1,msg.arg2,1),(PieceData)msg.obj);
                         doMove();
                         break;
                     default: break;
                 }
             }
         };
-        Board.handlerInit++;
-        Looper.loop();
+//        Board.handlerInit++;
+//        Looper.loop();
     }
     private void doMove() {
         if (numPieces < 3) {
@@ -36,9 +36,25 @@ public class PlayerTwo implements Runnable{
                 pieces = new PieceData[3];
             }
             pieces[numPieces] = getRandomPosition();
+            Board.movePiece(new PieceData(-1,-1, Board.BLUE), pieces[numPieces]);
+            updateBoard(new PieceData(-1,-1,Board.BLUE), pieces[numPieces]);
+            numPieces++;
             return;
         }
-        PieceData a
+        PieceData bestMove = findBestPossibleMove();
+        int pieceNum = -1;
+        for(int i = 0; i < 3; i++) {
+            if (bestMove.getID() == 3 && pieces[i].getX() != bestMove.getX()){
+                pieceNum = i;
+                break;
+            }
+            else if (bestMove.getID() == 4 && pieces[i].getY() != bestMove.getX())
+                pieceNum = i;
+            break;
+        }
+        PieceData newSpot = getBestPossibleOpening(bestMove);
+        updateBoard(pieces[pieceNum], newSpot);
+        Board.movePiece(pieces[pieceNum], newSpot);
     }
     private void updateBoard(PieceData oldPiece, PieceData newPiece) {
         if(newPiece == null) {
@@ -68,7 +84,46 @@ public class PlayerTwo implements Runnable{
         return new PieceData(x, y, 1);
     }
     public static void endLooper() {
-        playerOne.removeCallbacksAndMessages(null);
-        playerOne.getLooper().quit();
+        playerTwo.removeCallbacksAndMessages(null);
+        playerTwo.getLooper().quit();
     }
+   private PieceData findBestPossibleMove() {
+        PieceData best = new PieceData(-1,-1,-1);
+        for(int i = 0; i < 3; i++) {
+            int horizontal = 0;
+            int vertical = 0;
+            for (int j = 0; j < 3; j++){
+                horizontal += personalBoard[i][j];
+                vertical += personalBoard[j][i];
+            }
+            if(best.getY() < vertical) {
+                best.setX(i);
+                best.setY(vertical);
+                best.setID(4);
+                if(vertical == 2) {
+                    return best;
+                }
+            }
+            else if(best.getY() < horizontal) {
+                best.setX(i);
+                best.setY(horizontal);
+                best.setID(3);
+                if(horizontal == 2) {
+                    return best;
+                }
+            }
+       }
+        return best;
+   }
+   private PieceData getBestPossibleOpening(PieceData bestMove) {
+        for(int i = 0; i < 3; i++) {
+            if(bestMove.getID() == 3 && Board.getBoard()[bestMove.getX()][i] == Board.EMPTY) {
+                return new PieceData(bestMove.getX(), i, Board.BLUE);
+            }
+            else if(bestMove.getID() == 4 && Board.getBoard()[i][bestMove.getX()] == Board.EMPTY){
+                return new PieceData(i, bestMove.getX(), Board.BLUE);
+            }
+        }
+        return getRandomPosition();
+   }
 }
